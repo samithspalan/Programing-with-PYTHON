63c2191cc475fc9ac717c23d594e08c3fd08f0b5
samithspalan <samithpoojary12345@gmail.com>
2024-12-05 00:56:39 +0530

    python basics

---
+++ Day12/vowel.py
@@ -0,0 +1,6 @@
+name="aeioudfkdjkdghkfjklgkghfaeikjghlgklgjoi"
+count=0
+for i in name:
+    if i=="a" or i=="e" or i=="i" or i=="o" or i=="u":
+        count=count+1
+print(count)  
---
+++ Day12/vowel2.py
@@ -0,0 +1,7 @@
+vowel="aeiou"
+word="hello worldi"
+count=0
+for i in word:  
+  if i in vowel:
+    count+=1  
+print(count) 
