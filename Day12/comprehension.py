0cd75f4133cbc288982c288edae367b96d62a88e
samithspalan <samithpoojary12345@gmail.com>
2024-12-05 01:15:11 +0530

    python basics

---
+++ Day12/comprehension.py
@@ -0,0 +1,3 @@
+li=[2,4,5,6,9]
+gp=[i*i for i in li]
+print(gp)
---
+++ Day12/list.py
@@ -0,0 +1,10 @@
+items = {
+  "pen":25,
+  "pencil":10,
+  "book":20,
+  "sharpener":15,
+}
+spam=0
+for i in items:
+    spam+=items[i]
+print(spam) 
