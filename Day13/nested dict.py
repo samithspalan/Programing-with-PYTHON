646aaa088fd1669977c33fab46991ac0bd9a6205
samithspalan <samithpoojary12345@gmail.com>
2024-12-31 23:21:06 +0530

    python basics

---
+++ Day13/nested dict.py
@@ -0,0 +1,11 @@
+Mydict ={
+    "Fast":"in a quick manner",
+    "Harry":"a coder",
+    "newmydict":{'Harry':'player'}
+}
+print(Mydict['Fast'])
+print(Mydict["Harry"])
+
+print(Mydict['newmydict']['Harry'])
+print(list(Mydict.keys()))
+print(Mydict.keys())
