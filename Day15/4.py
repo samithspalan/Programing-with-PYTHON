7da2787c3dc106839d941e1212f845ab72109d63
samithspalan <samithpoojary12345@gmail.com>
2025-01-12 10:16:01 +0530

    initial commit

--- Day15/4.py
+++ Day15/4.py
@@ -1,5 +1,5 @@
-import send2trash
+#import send2trash
 baconFile = open('bacon.txt', 'a')   # creates the file
 baconFile.write('Bacon is not a vegetable.')
 baconFile.close()
-send2trash.send2trash('bacon.txt')
+#send2trash.send2trash('bacon.txt')
