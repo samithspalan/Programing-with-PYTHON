2e41bf13a27ffb5308db6216686b258ff66111c0
samithspalan <samithpoojary12345@gmail.com>
2025-01-19 16:45:47 +0530

    initial commit

---
+++ Day17/2.py
@@ -0,0 +1,14 @@
+class Database:
+    def __init__(self):
+        self.__storage = {}#private
+
+    def save_data(self, key, value):
+        self.__storage[key] = value
+        print(f"Data saved for {key}")
+
+    def get_data(self, key):
+        return self.__storage.get(key, "No data found")
+
+db = Database()
+db.save_data("user_101", {"name": "Raj", "age": 30})
+print(db.get_data("user_101"))
