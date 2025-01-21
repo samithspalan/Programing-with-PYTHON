8a0d84417fd3e8b24817a1f08a861e24839074c1
samithspalan <samithpoojary12345@gmail.com>
2025-01-21 19:28:33 +0530

    initial commit

---
+++ Day17/5.py
@@ -0,0 +1,15 @@
+class Animal:
+    def make_sound(self):
+        pass
+
+class Dog(Animal):
+    def make_sound(self):
+        print("Bark")
+
+class Cat(Animal):
+    def make_sound(self):
+        print("Meow")
+
+animals = [Dog(), Cat()]
+for animal in animals:
+    animal.make_sound()
---
+++ Day17/6.py
@@ -0,0 +1,15 @@
+class Notification:
+    def send(self):
+        pass
+
+class EmailNotification(Notification):
+    def send(self):
+        print("Sending Email")
+
+class SMSNotification(Notification):
+    def send(self):
+        print("Sending SMS")
+
+notifications = [EmailNotification(), SMSNotification()]
+for notification in notifications:
+    notification.send()
